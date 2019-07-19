3d17d8f0b6fcd966731224ebfc46992e2341f3f8
Administrator <yangzheng@himobi.net>
2019-07-19 22:54:32 +0800

    优化代码

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,18 +1,16 @@
 $().ready(function () {
+
     $('#Login').click(function () {
         var username = $('#username').val();
         var password = $('#password').val();
-        console.log("username="+username);
-        console.log("password="+password);
 
-        if ($('#username').val() == "" || $('#password').val() == "") {
+        if (username == "" || password == "") {
             alert("用户名或密码不能为空！");
-        }
-        else {
+        } else {
             $.ajax({
                 type: "POST",
                 url: "login",
-                data: "username=" + escape($('#username').val()) + "&password=" + escape($('#password').val()),
+                data: "username=" + escape(username) + "&password=" + escape(password),
                 beforeSend: function () {
                     $("#loading").css("display", "block"); //点击登录后显示loading，隐藏输入框
                     $("#login").css("display", "none");
@@ -20,16 +18,15 @@ $().ready(function () {
                 success: function (result) {
                     $("#loading").hide(); //隐藏loading
                     if (result.code == "200") {
-                        //parent.tb_remove();
-                        parent.document.location.href = "index.html"; //如果登录成功则跳到管理界面
-                        parent.tb_remove();
-                    }else {
+                        parent.document.location.href = "index.jsp"; //如果登录成功则跳到管理界面
+                    } else {
                         alert("登录失败！");
                     }
-                },
-                error: function (XMLHttpRequest, textStatus, thrownError) {
                 }
             });
         }
+
+
+
     });
 });
